20c26272e956965dd0caa8b0bec8f6f45af3ce8c
johnnyrpy <johnny@reepay.com>
2024-12-10 19:52:11 +0000

    Update embedded.js

--- embedded.js
+++ embedded.js
@@ -1,6 +1,15 @@
-var rp = new Reepay.EmbeddedSubscription(
-  "cs_00bfe691e9194265446ee9ee0ce6ebfa",
-  {
-    html_element: "rp_container",
-  }
-);
+var rp = new Reepay.EmbeddedCheckout(null, { html_element: "rp_container" }); // No session id given
+
+rp.addEventHandler(Reepay.Event.Accept, function (data) {
+  console.log("Success:", data);
+});
+
+rp.addEventHandler(Reepay.Event.Error, function (data) {
+  console.log("Error:", data);
+});
+
+rp.addEventHandler(Reepay.Event.Close, function (data) {
+  console.log("Close:", data);
+});
+
+rp.show("cs_00bfe691e9194265446ee9ee0ce6ebfa");
