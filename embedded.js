84e8368740c98e34047339e915abd8496892b291
johnnyrpy <johnny@reepay.com>
2024-12-17 21:30:17 +0000

    Improved embedded

--- embedded.js
+++ embedded.js
@@ -2,26 +2,14 @@ var rp;
 var urlParams = {};
 
 window.onload = function () {
-  rp = new Reepay.EmbeddedCheckout(null, { html_element: "rp_container" });
-
-  rp.addEventHandler(Reepay.Event.Accept, function (data) {
-    console.log("Success:", data);
-  });
-
-  rp.addEventHandler(Reepay.Event.Error, function (data) {
-    console.log("Error:", data);
-  });
-
-  rp.addEventHandler(Reepay.Event.Close, function (data) {
-    console.log("Close:", data);
-  });
+  init();
 
   setTimeout(() => {
     initSession();
-  }, 1000);
+  }, 500);
 };
 
-function initSession() {
+function init() {
   var urlQueries = window.location.href.split("?")[1];
   if (urlQueries) {
     urlQueries = urlQueries.split("&");
@@ -33,8 +21,35 @@ function initSession() {
     });
   }
 
-  if (urlParams["session"]) {
-    rp.show(urlParams["session"]);
+  let environment = urlParams["env"];
+  let reepayScriptUrl = "https://checkout.reepay.com/checkout.js";
+  if (environment === "staging") {
+    reepayScriptUrl = "https://staging-checkout.reepay.com/checkout.js";
+  }
+
+  const script = document.createElement("script");
+  script.src = reepayScriptUrl;
+  script.async = true;
+  script.onload = function () {
+    rp = new Reepay.EmbeddedCheckout(null, { html_element: "rp_container" });
+
+    rp.addEventHandler(Reepay.Event.Accept, function (data) {
+      console.log("Success:", data);
+    });
+    rp.addEventHandler(Reepay.Event.Error, function (data) {
+      console.log("Error:", data);
+    });
+    rp.addEventHandler(Reepay.Event.Close, function (data) {
+      console.log("Close:", data);
+    });
+  };
+  window.document.head.appendChild(script);
+}
+
+function initSession() {
+  let session = urlParams["session"];
+  if (session) {
+    rp.show(session);
 
     Reepay.isGooglePayAvailable().then((isGooglePayAvailable) => {
       console.log("isGooglePayAvailable:", isGooglePayAvailable);
